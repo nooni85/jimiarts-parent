754d80d2f31fbf6e9f25e297f70ed4b3ebe651da
Sangwoo Joo <nooni85@naver.com>
2022-03-08 00:04:35 +0900

    Update websecurity

--- cwi/src/main/java/com/jimiarts/cwi/JimiartsApplication.java
+++ cwi/src/main/java/com/jimiarts/cwi/JimiartsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication(scanBasePackages = "com.jimiarts.models")
+@SpringBootApplication(scanBasePackages = {"com.jimiarts.models", "com.jimiarts.cwi"})
 public class JimiartsApplication {
 
     public static void main(String[] args) {
---
+++ cwi/src/main/java/com/jimiarts/cwi/config/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.jimiarts.cwi.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/**").permitAll();
+    }
+}
