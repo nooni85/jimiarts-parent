1e50c57df91e69e04073b3b3a4b6cf346475ce5c
Sangwoo Joo <nooni85@naver.com>
2022-03-25 17:24:03 +0900

    Update: views

--- cwi/src/test/java/com/jimiarts/cwi/controller/AuthControllerTest.java
+++ cwi/src/test/java/com/jimiarts/cwi/controller/AuthControllerTest.java
@@ -1,5 +1,6 @@
 package com.jimiarts.cwi.controller;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -18,6 +19,7 @@ class AuthControllerTest {
     private MockMvc mockMvc;
 
     @Test
+    @DisplayName("멤버 조인 테스트")
     public void joinTest() throws Exception {
         assertTrue(mockMvc.perform(get("/auth/login"))
                 .andExpect(status().isOk())
