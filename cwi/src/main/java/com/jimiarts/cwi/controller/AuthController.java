16dceed0fcd193bb84e967d7d7b224f4870e3c7c
Sangwoo Joo <nooni85@naver.com>
2022-04-12 23:06:04 +0900

    Noti

--- cwi/src/main/java/com/jimiarts/cwi/controller/AuthController.java
+++ cwi/src/main/java/com/jimiarts/cwi/controller/AuthController.java
@@ -1,18 +1,26 @@
 package com.jimiarts.cwi.controller;
 
 import com.jimiarts.cwi.form.MemberForm;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/auth")
 public class AuthController {
+    final Logger log = LoggerFactory.getLogger(AuthController.class);
+
     @GetMapping("/login")
     public String login() {
         return "auth/login";
@@ -24,16 +32,19 @@ public class AuthController {
     }
 
     @GetMapping("/join")
-    public String join() {
+    public String join(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
         return "auth/join";
     }
 
     @PostMapping("/join")
-    public String doJoin(@Valid MemberForm memberForm, BindingResult bindingResult, Model model) {
+    public String doJoin(@Validated MemberForm memberForm, BindingResult bindingResult, Model model) {
         if (bindingResult.hasErrors()) {
-            model.addAttribute("member", memberForm);
+            model.addAttribute("memberForm", memberForm);
             return "auth/join";
         }
+
+        model.addAttribute("memberForm", memberForm);
         return "auth/join";
     }
 }
--- cwi/src/main/java/com/jimiarts/cwi/form/MemberForm.java
+++ cwi/src/main/java/com/jimiarts/cwi/form/MemberForm.java
@@ -1,8 +1,29 @@
 package com.jimiarts.cwi.form;
 
-import javax.validation.constraints.NotEmpty;
+import com.jimiarts.cwi.validator.PasswordValueMatch;
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@PasswordValueMatch.List({
+    @PasswordValueMatch(
+        field = "password",
+        fieldMatch = "passwordConfirmation",
+        message = "Passwords do not match!"
+    )
+})
+@Getter @Setter
 public class MemberForm {
     @NotEmpty(message = "username cannot be empty")
     private String username;
+
+    @NotEmpty(message = "password cannot be empty")
+    @NotNull
+    private String password;
+
+    @NotEmpty(message = "passwordConfirmation cannot be empty")
+    @NotNull
+    private String passwordConfirmation;
 }
